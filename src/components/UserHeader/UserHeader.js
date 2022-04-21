3547b4764d5877912b8e37c5e6425ee832fa88e4
Felicio <guifelicio7@gmail.com>
2022-04-21 16:04:30 -0300

    correção de titulo do userHeader

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,32 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import { useLocation } from 'react-router-dom';
-import UserHeaderNav from '../UserHeaderNav/UserHeaderNav';
-import styles from './userHeader.module.css';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import UserHeaderNav from "../UserHeaderNav/UserHeaderNav";
+import styles from "./userHeader.module.css";
 
 const UserHeader = () => {
   const location = useLocation();
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   useEffect(() => {
     const { pathname } = location;
+
     switch (pathname) {
-      case '/conta/estatisticas':
-        setTitle('Estatísticas');
-      case '/conta/postar':
-        setTitle('Poste sua foto');
-      break;
+      case "/conta/estatisticas":
+        setTitle("Estatísticas");
+        break;
+      case "/conta/postar":
+        setTitle("Poste sua foto");
+        break;
       default:
-          setTitle('Minha Conta');
+        setTitle("Minha Conta");
         break;
     }
   }, [location]);
-  
 
   return (
     <header className={styles.header}>
-      <h1 className='title'>{title}</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
-  )
-}
+  );
+};
 
-export default UserHeader
+export default UserHeader;
